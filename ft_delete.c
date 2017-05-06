6f2685ab289d0847486c864d99fb7b0490ed13e2
myrrh <myrrh@austincd.m-m>
2017-05-05 23:28:13 -0700

    deletion

--- ft_delete.c
+++ ft_delete.c
@@ -1,7 +1,34 @@
 #include "ft_db.h"
 
 long long	ft_delete_record(t_dbinfo *dbmeta, int fd, long long record_id)
-{}
+{
+	int		counter;
+	t_entry	*empty;
 
-long long	ft_delete_field(t_dbinfo *dbmeta, int fd, long long record_id)
-{}
+	empty = (t_entry*)ft_memalloc(sizeof(t_entry));
+	counter = 0;
+	if (dbmeta)
+	{
+		while (counter < dbmeta->num_fields)
+		{
+			ft_write_entry(dbmeta, fd, empty, dbmeta->records[record_id]->entries[counter]);
+			++counter;
+		}
+	}
+}
+
+long long	ft_delete_field(t_dbinfo *dbmeta, int fd, long long field_id)
+{	int		counter;
+	t_entry	*empty;
+
+	empty = (t_entry*)ft_memalloc(sizeof(t_entry));
+	counter = 0;
+	if (dbmeta)
+	{
+		while (counter < dbmeta->num_records)
+		{
+			ft_write_entry(dbmeta, fd, empty, dbmeta->fields[field_id]->entries[counter]);
+			++counter;
+		}
+	}
+}
